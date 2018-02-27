7a80bd88441029b9c5f3ee9ff3dbf654b2058512
David Moran <ddavidmorang@gmail.com>
2018-02-27 23:54:58 +0100

    Remove weight from graphs

--- semana-3/declaracion.cpp
+++ semana-3/declaracion.cpp
@@ -7,15 +7,11 @@ const int MAXN = 1000;
 int N,E;
 
 struct edge{
-  int from, to, weight;
+  int from, to;
   edge(){}
-  edge(int a, int b, int c){
+  edge(int a, int b){
     from = a;
     to = b;
-    weight = c;
-  }
-  bool operator<(const edge &other)const{ // sobrecarga de operadores para ordenar
-    return weight < other.weight;
   }
 };
 
@@ -27,10 +23,9 @@ int main(){
 	scanf("%d %d",&N,&E);
   for(int i=1;i<=N;i++) graph[i].clear(); // limpia el grafo
 	for(int i=0;i<E;i++){
-    int from, to, weight; scanf("%d %d %d",&from, &to, &weight);
-    graph[from].push_back(edge(from,to,weight));
-    graph[to].push_back(edge(to,from,weight)); // borrar linea si es dirigido
-    graph_m[from][to] = graph_m[to][from] = weight;
+    int from, to, weight; scanf("%d %d",&from, &to);
+    graph[from].push_back(edge(from,to));
+    graph[to].push_back(edge(to,from)); // borrar linea si es dirigido
 	}
 	return 0;
 }
